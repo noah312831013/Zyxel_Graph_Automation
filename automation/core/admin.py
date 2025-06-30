4a4b063032ce1d76f6903e17f9b3676a07c62a0a
Noah Lin <noah.ai12@nycu.edu.tw>
2025-06-30 09:59:47 +0800

    celery beat

--- automation/core/admin.py
+++ automation/core/admin.py
@@ -1,8 +1,4 @@
 from django.contrib import admin
 from .models import UserToken
-from django_celery_beat.models import PeriodicTask, IntervalSchedule
 # Register your models here.
 admin.site.register(UserToken)
-
-admin.site.register(PeriodicTask)
-admin.site.register(IntervalSchedule)
